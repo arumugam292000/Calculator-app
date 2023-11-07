ebdd4ddad8487a850adfb62278032edd819749d5
arumugam292000 <6face2000govindan@gmail.com>
2023-11-07 18:11:41 +0530

    ta

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-
   const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
 
   const handleButtonClick = (value) => {
-    setInput(input + value);
-  };
-
-  const handleCalculate = () => {
-    try {
-      setInput(eval(input) || 'Error');
-    } catch (error) {
-      setInput('Error');
+    if (value === '=') {
+      try {
+        setResult(eval(input) || 'Error');
+      } catch (error) {
+        setResult('Error');
+      }
+      setInput('');
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
     }
-  };
-
-  const handleClear = () => {
-    setInput('');
   };
 
   return (
@@ -27,32 +27,41 @@ function App() {
         <input type="text" value={input} disabled />
       </div>
       <div className="buttons">
-      <button onClick={handleClear}>AC</button>
-      
-      <button onClick={() => handleButtonClick('+')}>+</button>
-      <button onClick={() => handleButtonClick('-')}>-</button>
-      <button onClick={() => handleButtonClick('*')}>*</button>
-      <button onClick={() => handleButtonClick('/')}>/</button>
-      <button onClick={() => handleButtonClick('%')}>%</button>
-        <button onClick={() => handleButtonClick('7')}>7</button>
-        <button onClick={() => handleButtonClick('8')}>8</button>
-        <button onClick={() => handleButtonClick('9')}>9</button>
-        <button onClick={() => handleButtonClick('4')}>4</button>
-        <button onClick={() => handleButtonClick('5')}>5</button>
-        <button onClick={() => handleButtonClick('6')}>6</button>
-        <button onClick={() => handleButtonClick('1')}>1</button>
-        <button onClick={() => handleButtonClick('2')}>2</button>
-        <button onClick={() => handleButtonClick('3')}>3</button>
-        <button onClick={() => handleButtonClick('0')}>0</button>
-        <button onClick={() => handleButtonClick('00')}>00</button>
-        <button onClick={() => handleButtonClick('.')}>.</button>
-        <button onClick={handleCalculate}>=</button>
-        <button onClick={handleClear}>on/off</button>
-  
+        <div className="row">
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button onClick={() => handleButtonClick('/')}>/</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button onClick={() => handleButtonClick('*')}>*</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button onClick={() => handleButtonClick('-')}>-</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('0')}>0</button>
+          <button onClick={() => handleButtonClick('.')}>.</button>
+          <button onClick={() => handleButtonClick('=')}>=</button>
+          <button onClick={() => handleButtonClick('+')}>+</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('C')}>C</button>
+        </div>
+      </div>
+      <div className="result">
+        <span>{result}</span>
       </div>
     </div>
   );
 }
 
 export default App;
+
 
